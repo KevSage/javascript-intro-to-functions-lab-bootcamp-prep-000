a72653098275eaac6f6a379aa867da55258c4740
Kevin Sage <ksagedeveloper@gmail.com>
2019-09-26 02:27:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function sayHiToGrandma(string) {
   } else if {
     string === string.toUpperCase()) {
       return "YES INDEED!";
-  } else {
+  }} else {
     if (string === "I love you, Grandma.") {
       return ("I love you, too.");
   }
